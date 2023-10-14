4207263073ca842fa0cf6aeafd4d368e1305ef42
Rodrigo Ferreira <103112629+RodrigoLF6@users.noreply.github.com>
2023-10-14 15:27:15 +0100

    3rd

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ function handleLogin(event) {
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
   const rememberMe = document.getElementById('remember').checked;
-
+  
   // Lógica de login (substitua pelo seu código de autenticação)
   console.log('Login:', username, ' Password:', password, ' Remember Me:', rememberMe);
-
-  // Redirecionar para a página do dashboard (simulando)
-  window.location.href = 'dashboard.html';
+  
+  // Redirecionar para a página do select
+  window.location.href = 'select.html';
 }
 
 // Função para lidar com o registo
@@ -17,12 +17,12 @@ function handleRegister(event) {
   event.preventDefault();
   const newUsername = document.getElementById('newUsername').value;
   const newPassword = document.getElementById('newPassword').value;
-
+  
   // Lógica de registo (substitua pelo seu código de registo)
   console.log('Novo Utilizador:', newUsername, ' Nova Palavra-passe:', newPassword);
-
-  // Redirecionar para a página do dashboard (simulando)
-  window.location.href = 'dashboard.html';
+  
+  // Redirecionar para a página de select 
+  window.location.href = 'select.html';
 }
 
 // Adicionar os event listeners aos formulários
@@ -36,6 +36,82 @@ if (loginForm) {
 if (registerForm) {
   registerForm.addEventListener('submit', handleRegister);
 }
+// Seleção de moedas
+document.getElementById('coinSelectionForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  const selectedCoins = [];
+  document.querySelectorAll('input[name="coins"]:checked').forEach(checkbox => {
+      selectedCoins.push(checkbox.value);
+  });
+  localStorage.setItem('favoriteCoins', JSON.stringify(selectedCoins));
+  window.location.href = 'dashboard.html';
+});
+
+// Dashboard
+const coinData = [
+  { name: "Bitcoin", exchangeRate: "€45,000", recommendedSite: "example1.com" },
+  // ... outros dados ...
+];
+
+const dashboardTable = document.getElementById('dashboardCoins');
+coinData.forEach(coin => {
+  const row = document.createElement('tr');
+  row.innerHTML = `
+      <td>${coin.name}</td>
+      <td>${coin.exchangeRate}</td>
+      <td><a href="http://${coin.recommendedSite}" target="_blank">${coin.recommendedSite}</a></td>
+  `;
+  dashboardTable.appendChild(row);
+});
+
+// Perfil
+const favoriteCoins = JSON.parse(localStorage.getItem('favoriteCoins') || '[]');
+const favoriteCoinsList = document.getElementById('favoriteCoinsList');
+favoriteCoins.forEach(coin => {
+  const listItem = document.createElement('li');
+  listItem.textContent = coin;
+  favoriteCoinsList.appendChild(listItem);
+});
+
+// Notícias
+const newsData = [
+  { title: "Bitcoin atinge novo recorde!", content: "Bitcoin atingiu um novo recorde de €50,000 hoje." },
+  // ... outras notícias ...
+];
+
+const newsContainer = document.getElementById('newsContainer');
+newsData.forEach(news => {
+  const newsDiv = document.createElement('div');
+  newsDiv.innerHTML = `
+      <h3>${news.title}</h3>
+      <p>${news.content}</p>
+  `;
+  newsContainer.appendChild(newsDiv);
+});
+// ... scripts já fornecidos anteriormente ...
+
+// Gráficos
+// Nota: Este é um exemplo simplificado. Na prática, você usaria uma biblioteca como Chart.js ou D3.js para renderizar gráficos reais.
+
+const chartData = [
+  { title: "Bitcoin", data: [/* ... dados de preços ... */] },
+  { title: "Ethereum", data: [/* ... dados de preços ... */] },
+  // ... outros dados de moedas ...
+];
+
+const chartsContainer = document.getElementById('chartsContainer');
+chartData.forEach(chart => {
+  const chartDiv = document.createElement('div');
+  chartDiv.className = 'chart';
+  chartDiv.innerHTML = `
+      <div class="chart-title">${chart.title}</div>
+      <!-- Aqui, o gráfico real seria renderizado -->
+  `;
+  chartsContainer.appendChild(chartDiv);
+});
+
+
+//FIREBASE
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
